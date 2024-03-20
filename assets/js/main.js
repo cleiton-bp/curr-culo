27c81f5b0ca29802e3bb6ab7fa832a91c9e30cc8
Cleiton Becker Pereira <114703960+cleiton-bp@users.noreply.github.com>
2024-03-20 09:23:11 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ btn_printer.addEventListener("click", (evt) => {
     alert("Você será redirecionado!!")
   setTimeout(() => {
     window.open(
-      "https://drive.google.com/file/d/1NgT3cByuqWT2bI2cb6Ak_yjo7CQn-GnZ/view?usp=drive_link",
+      "https://drive.google.com/file/d/1YPGI4XfqNRgo1aFRKyQGoFPMvruqaQC4/view",
       "_blank"
     );
   }, 3000);
